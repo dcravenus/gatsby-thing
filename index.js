808967e7545cc0ea7a66ec984b051faf5ccc5987
derrick <dcravenus@gmail.com>
2020-08-01 16:07:04 -0600

    Update styles

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const fs = require("fs");
 const moment = require("moment");
 const { getNYTData } = require("./fetchNYT");
 const { getSLTribData } = require("./fetchSLTrib");
-const { getNewYorkerData } = require("./fetchNewYorker");
 
 const generateIndexHTML = async () => {
   const nytData = await getNYTData();
   const sltribData = await getSLTribData();
-  const newYorkerData = await getNewYorkerData();
 
-  let fileData = "<link href='styles.css' rel='stylesheet'></style>";
-  fileData += `<h1>${moment().format("dddd, MMMM Do, YYYY")}<h1>`;
+  let fileData = `
+    <!doctype html>
+      <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='styles.css' rel='stylesheet'></style>
+        </head>
+        <body>
+  `;
+  fileData += `<h1>${moment().format("dddd, MMMM Do, YYYY")}<h1><hr>`;
   fileData += nytData;
+  fileData += "<hr>";
   fileData += sltribData;
-  fileData += newYorkerData;
+
+  fileData += `</body></html>`;
 
   fs.writeFile("index.html", fileData, (err) => {
     if (err) {
