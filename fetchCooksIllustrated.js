1dd75df374bafee2a860b871ed4201becd464a82
derrick <dcravenus@gmail.com>
2020-08-21 16:20:41 -0600

    Update

--- fetchCooksIllustrated.js
+++ fetchCooksIllustrated.js
@@ -2,22 +2,32 @@ const puppeteer = require("puppeteer");
 const { JSDOM } = require("jsdom");
 
 exports.getCooksIllustratedData = async () => {
-  const url = `https://oremut.rbdigital.com/magazine/5455/468177`;
+  const url = `https://oremut.rbdigital.com/all-issues/emagazine/5455`;
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+  await page.waitForSelector(".title-card a");
+  let text = await page.evaluate(() => document.documentElement.outerHTML);
+
+  let dom = new JSDOM(text);
+  let document = dom.window.document;
+
+  const magazineURL =
+    "https://oremut.rbdigital.com/" +
+    document.querySelector(".title-card a").href;
+  await page.goto(magazineURL);
   await page.waitForSelector(".product-detail-date p:last-of-type");
-  const text = await page.evaluate(() => document.documentElement.outerHTML);
+  text = await page.evaluate(() => document.documentElement.outerHTML);
 
   await browser.close();
 
-  const dom = new JSDOM(text);
-  const document = dom.window.document;
+  dom = new JSDOM(text);
+  document = dom.window.document;
 
   const issueDate = document.querySelector(
     ".product-detail-date p:last-of-type"
   ).textContent;
 
-  return { issueDate, url };
+  return { issueDate, url: magazineURL };
 };
